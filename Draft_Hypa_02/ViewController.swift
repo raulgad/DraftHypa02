fe6bcf3799ea9ae0c44121bd957e87bfa14165e9
Raul <mac@macbook-pro-mac.local>
2016-07-18 16:39:31 +0300

    Set UISnapBehavior to views and remove all behaviors from animator in .began pan

--- Draft_Hypa_02/ViewController.swift
+++ Draft_Hypa_02/ViewController.swift
@@ -9,106 +9,154 @@
 import UIKit
 
 class ViewController: UIViewController, UIGestureRecognizerDelegate{
-    var preludeView = UIView()
-    var answerTopView: UIView!
-    var answerBottomView: UIView!
+    var prelude: UIView!
+    var answerTop: UIView!
+    var answerBottom: UIView!
 
+    var defaultViewCenter: CGPoint!
+    
+    var animator: UIDynamicAnimator!
+    //var dynamicItemBehavior: UIDynamicItemBehavior!
+    
+    var preludePanGestureRecognizer: UIPanGestureRecognizer!
+    var answerTopPanGestureRecognizer: UIPanGestureRecognizer!
+    var answerBottomPanGestureRecognizer: UIPanGestureRecognizer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         initViews()
         createConstrants()
+        animator = UIDynamicAnimator(referenceView: view)
         
-        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handleTap(recognizer:)))
-        //panGestureRecognizer.delegate = self
-        preludeView.addGestureRecognizer(panGestureRecognizer)
+        //Create UIPanGestureRecognizer
+        preludePanGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(pan(pan:)))
+        answerTopPanGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(pan(pan:)))
+        answerBottomPanGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(pan(pan:)))
+        
+        //Set delegate to UIPanGestureRecognizer
+        preludePanGestureRecognizer.delegate = self
+        answerTopPanGestureRecognizer.delegate = self
+        answerBottomPanGestureRecognizer.delegate = self
+        
+        //Set UIPanGestureRecognizer to views
+        prelude.addGestureRecognizer(preludePanGestureRecognizer)
+        answerTop.addGestureRecognizer(answerTopPanGestureRecognizer)
+        answerBottom.addGestureRecognizer(answerBottomPanGestureRecognizer)
+        
+        //MARK: Why we cant set isExclusiveTouch only for one view (in pan.view) and must setting for all views?
+        //Turn off multi touch for views
+        prelude.isExclusiveTouch = true
+        answerTop.isExclusiveTouch = true
+        answerBottom.isExclusiveTouch = true
+        
+        //FIXME: Not sure this assignments should be there, maybe in pan.ended
+        //Set UIDynamicItemBehavior to views
+        
     }
     
     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
         return true
     }
     
-    func handleTap(recognizer: UIPanGestureRecognizer) {
-        let translation = recognizer.translation(in: preludeView)
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+//    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+//        return (animator.isRunning ? false : true)
+//    }
+    
+    func pan(pan: UIPanGestureRecognizer) {
+        let translation = pan.translation(in: self.view)
+        guard let panView = pan.view else {
+            print("Error with unwrap pan.view")
+            return
+        }
         
-        if recognizer.state == UIGestureRecognizerState.began {
+        switch pan.state {
+            case .began:
+                //FIXME: Hardcoding
+                defaultViewCenter = panView.center //CGPoint(x: 160, y: 108)
+                //animator.removeAllBehaviors()
             
-        }
-        else if recognizer.state == UIGestureRecognizerState.changed {
-            if let view = recognizer.view {
-                view.center = CGPoint(x: view.center.x + translation.x, y: view.center.y)
-            }
-            recognizer.setTranslation(CGPoint.zero, in: preludeView)
-        }
-        else if recognizer.state == UIGestureRecognizerState.ended {
-            //Figure out the length of the velocity vector (i.e. the magnitude)
-            let velocity = recognizer.velocity(in: preludeView)
-            let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
-            let slideMultiplier = magnitude / 200
+                //print(".began animator.behaviors: \(animator.behaviors)")
+
+            case .changed:
+                panView.center = CGPoint(x: panView.center.x + translation.x, y: panView.center.y)
+                pan.setTranslation(CGPoint.zero, in: panView)
             
-            //If the length is < 200, then decrease the base speed, otherwise increase it.
-            let slideFactor = 0.1 * slideMultiplier
+            case .cancelled, .ended:
+                //Add UIDynamicItemBehavior to animator
+                let dynamicItemBehavior = UIDynamicItemBehavior(items: [panView])
+                dynamicItemBehavior.allowsRotation = false
+                animator.addBehavior(dynamicItemBehavior)
+                
+                //Set UISnapBehavior to views
+                let snapBehavior = UISnapBehavior(item: panView, snapTo: defaultViewCenter)
+                snapBehavior.damping = 0.15
+                animator.addBehavior(snapBehavior)
+
             
-            //Calculate a final point based on the velocity and the slideFactor.
-            var finalPoint = CGPoint(x: (recognizer.view?.center.x)! + (velocity.x * slideFactor), y: (recognizer.view?.center.y)!)
+//                snapBehavior.action = {
+//                    print(panView.center)
+//                }
             
-            //Make sure the final point x is within the view’s bounds
-            finalPoint.x = min(max(finalPoint.x, 0), preludeView.bounds.size.width)
             
-            //Animate the view to the final resting place.
-            UIView.animate(withDuration: Double(slideFactor*2),
-                           delay: 0,
-                           options: UIViewAnimationOptions.curveEaseOut,
-                           animations: {
-                            recognizer.view?.center = finalPoint
-                },
-                           completion: nil)
+                //print(".ended animator.behaviors: \(animator.behaviors)")
+                //print("________________________________________________")
+            
+            default: ()
         }
     }
-
+    
     func initViews() {
         //Init
-        preludeView = UIView()
-        answerTopView = UIView()
-        answerBottomView = UIView()
+        prelude = UIView()
+        answerTop = UIView()
+        answerBottom = UIView()
         
         //Prepare Auto Layout
-        preludeView.translatesAutoresizingMaskIntoConstraints = false
-        answerTopView.translatesAutoresizingMaskIntoConstraints = false
-        answerBottomView.translatesAutoresizingMaskIntoConstraints = false
+        prelude.translatesAutoresizingMaskIntoConstraints = false
+        answerTop.translatesAutoresizingMaskIntoConstraints = false
+        answerBottom.translatesAutoresizingMaskIntoConstraints = false
         
         //Set background color
-        preludeView.backgroundColor = UIColor.blue()
-        answerTopView.backgroundColor = UIColor.darkGray()
-        answerBottomView.backgroundColor = UIColor.lightGray()
+        prelude.backgroundColor = UIColor.blue()
+        answerTop.backgroundColor = UIColor.darkGray()
+        answerBottom.backgroundColor = UIColor.lightGray()
+        
+        //Set view's corner radius
+        answerTop.layer.cornerRadius = 4
+        answerBottom.layer.cornerRadius = 4
         
         //Add to super view
-        self.view.addSubview(preludeView)
-        self.view.addSubview(answerTopView)
-        self.view.addSubview(answerBottomView)
+        self.view.addSubview(prelude)
+        self.view.addSubview(answerTop)
+        self.view.addSubview(answerBottom)
     }
     
     func createConstrants() {
         //Create constrants preludeView
-        let pinLeftPreludeView = NSLayoutConstraint(item: preludeView, attribute: NSLayoutAttribute.left, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.left, multiplier: 1.0, constant: 0)
-        let pinTopPreludeView = NSLayoutConstraint(item: preludeView, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.top, multiplier: 1.0, constant: 24)
-        let pinRightPreludeView = NSLayoutConstraint(item: preludeView, attribute: NSLayoutAttribute.right, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.right, multiplier: 1.0, constant: 0)
-        let heightPreludeView = NSLayoutConstraint(item: preludeView, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.height, multiplier: 0.35, constant: 0)
+        let pinLeftPrelude = NSLayoutConstraint(item: prelude, attribute: NSLayoutAttribute.left, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.left, multiplier: 1.0, constant: 0)
+        let pinTopPrelude = NSLayoutConstraint(item: prelude, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.top, multiplier: 1.0, constant: 24)
+        let pinRightPrelude = NSLayoutConstraint(item: prelude, attribute: NSLayoutAttribute.right, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.right, multiplier: 1.0, constant: 0)
+        let heightPrelude = NSLayoutConstraint(item: prelude, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.height, multiplier: 0.35, constant: 0)
         
         //Create constraints answerTopView
-        let pinLeftAnswerTopView = NSLayoutConstraint(item: answerTopView, attribute: NSLayoutAttribute.left, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.left, multiplier: 1.0, constant: 12)
-        let pinRightAnswerTopView = NSLayoutConstraint(item:answerTopView, attribute: NSLayoutAttribute.right, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.right, multiplier: 1.0, constant: -12)
-        let topMarginAnswerTopViewToPreludeView = NSLayoutConstraint(item: answerTopView, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: preludeView, attribute: NSLayoutAttribute.bottom, multiplier: 1.0, constant: 12)
-        let heightAnswerTopView = NSLayoutConstraint(item: answerTopView, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.height, multiplier: 0.25, constant: 0)
+        let pinLeftAnswerTop = NSLayoutConstraint(item: answerTop, attribute: NSLayoutAttribute.left, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.left, multiplier: 1.0, constant: 12)
+        let pinRightAnswerTop = NSLayoutConstraint(item:answerTop, attribute: NSLayoutAttribute.right, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.right, multiplier: 1.0, constant: -12)
+        let topMarginAnswerTopToPrelude = NSLayoutConstraint(item: answerTop, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: prelude, attribute: NSLayoutAttribute.bottom, multiplier: 1.0, constant: 12)
+        let heightAnswerTop = NSLayoutConstraint(item: answerTop, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.height, multiplier: 0.25, constant: 0)
         
         //Create constrants answerBottomView
-        let pinLeftAnswerBottomView = NSLayoutConstraint(item: answerBottomView, attribute: NSLayoutAttribute.left, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.left, multiplier: 1.0, constant: 12)
-        let pinRightAnswerBottomView = NSLayoutConstraint(item: answerBottomView, attribute: NSLayoutAttribute.right, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.right, multiplier: 1.0, constant: -12)
-        let topMarginAnswerBottomViewToAnswerTopView = NSLayoutConstraint(item: answerBottomView, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: answerTopView, attribute: NSLayoutAttribute.bottom, multiplier: 1.0, constant: 12)
-        let heightAnswerBottomView = NSLayoutConstraint(item: answerBottomView, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.height, multiplier: 0.25, constant: 0)
+        let pinLeftAnswerBottom = NSLayoutConstraint(item: answerBottom, attribute: NSLayoutAttribute.left, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.left, multiplier: 1.0, constant: 12)
+        let pinRightAnswerBottom = NSLayoutConstraint(item: answerBottom, attribute: NSLayoutAttribute.right, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.right, multiplier: 1.0, constant: -12)
+        let topMarginAnswerBottomToAnswerTop = NSLayoutConstraint(item: answerBottom, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: answerTop, attribute: NSLayoutAttribute.bottom, multiplier: 1.0, constant: 12)
+        let heightAnswerBottom = NSLayoutConstraint(item: answerBottom, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.height, multiplier: 0.25, constant: 0)
         
         //Adding to super view
-        self.view.addConstraints([pinLeftPreludeView, pinTopPreludeView, pinRightPreludeView, heightPreludeView, pinLeftAnswerTopView, pinRightAnswerTopView, heightAnswerTopView, topMarginAnswerTopViewToPreludeView, pinLeftAnswerBottomView, pinRightAnswerBottomView, topMarginAnswerBottomViewToAnswerTopView, heightAnswerBottomView])
+        self.view.addConstraints([pinLeftPrelude, pinTopPrelude, pinRightPrelude, heightPrelude, pinLeftAnswerTop, pinRightAnswerTop, heightAnswerTop, topMarginAnswerTopToPrelude, pinLeftAnswerBottom, pinRightAnswerBottom, topMarginAnswerBottomToAnswerTop, heightAnswerBottom])
     }
     
 }
